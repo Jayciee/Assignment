ed443cdf12872612942919886b47e0274da3c131
jkarlokc <jkarlokc@gmail.com>
2021-08-04 12:47:17 +0100

    StableVersion0.1 Can currrently display list of monsters

---
+++ MonsterHunterJournal/BusinessLayer/RecordManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public partial class RecordManager
+    {
+        public List<Record> RetrieveAllRecords()
+        {
+            using (var db = new MonsterHunterJournalDBContext())
+            {
+                return db.Records.ToList();
+            }
+        }
+
+        public void addNewRecord() 
+        {
+            using (var db = new MonsterHunterJournalDBContext())
+            {
+                
+            }
+        }
+    }
+}
