435a91ac1ee2d249d5bf423f959b7b57b10846e6
jkarlokc <jkarlokc@gmail.com>
2021-08-08 21:34:41 +0100

    RecordedSizeCollapsesAndIsVisibleWhenApprorpiate

--- MonsterHunterJournal/WPFGUILayer/Views/CreateNewRecordView.xaml.cs
+++ MonsterHunterJournal/WPFGUILayer/Views/CreateNewRecordView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Controls;
+using System.Windows;
 using WPFGUILayer.ViewModels;
 using BusinessLayer;
 using System.Text.RegularExpressions;
@@ -17,6 +18,7 @@ namespace WPFGUILayer.Views
         private MonsterManager _mm = new MonsterManager();
         private RecordManager _rm = new RecordManager();
         private WeaponManager _wm = new WeaponManager();
+        bool _Successful = false;
         public CreateNewRecordView()
         {
             InitializeComponent();
@@ -47,6 +49,30 @@ namespace WPFGUILayer.Views
             }
             minutesTakenComboBox.ItemsSource = minutes;
             secondsTakenComboBox.ItemsSource = seconds;
+            
+        }
+
+        private void succeedListBoxItem_Selected(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _Successful = true;
+            if (recordedSizeTextBox.Visibility == Visibility.Collapsed)
+            {
+                recordedSizeTextBox.Visibility = Visibility.Visible;
+            }
+            
+        }
+
+        private void failedListBoxItem_Selected(object sender, System.Windows.RoutedEventArgs e)
+        {
+            
+            if (_Successful)
+            {
+                _Successful = false;
+                if (recordedSizeTextBox.Visibility == Visibility.Visible)
+                {
+                    recordedSizeTextBox.Visibility = Visibility.Collapsed;
+                }
+            }
             
         }
         //public CreateNewRecordView()
