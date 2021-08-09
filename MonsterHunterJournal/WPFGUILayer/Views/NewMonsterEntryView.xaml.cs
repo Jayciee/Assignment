220d718cead4db74807b183b07d03f61eb682759
jkarlokc <jkarlokc@gmail.com>
2021-08-09 21:14:06 +0100

    Added Methods (untested) to Create new Monster Entry

--- MonsterHunterJournal/WPFGUILayer/Views/NewMonsterEntryView.xaml.cs
+++ MonsterHunterJournal/WPFGUILayer/Views/NewMonsterEntryView.xaml.cs
@@ -1,5 +1,9 @@
 ﻿using System.Windows.Controls;
 using WPFGUILayer.ViewModels;
+using BusinessLayer;
+using System.Collections.Generic;
+using System.Windows;
+using System;
 
 namespace WPFGUILayer.Views
 {
@@ -8,10 +12,33 @@ namespace WPFGUILayer.Views
     /// </summary>
     public partial class NewMonsterEntryView : UserControl
     {
+        MonsterManager _mm = new MonsterManager();
         public NewMonsterEntryView()
         {
             InitializeComponent();
             this.DataContext = new NewMonsterEntryViewModel("MONSTER ENTRY HERE");
+        }
+        public void PopulateThreatLevelComboBox()
+        {
+            List<int> list = new List<int>();
+            for (int i = 1; i < 8; i++)
+            {
+                list.Add(i);
+            }
+            threatLevelComboBox.ItemsSource = list;
+        }
+        private void submitBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _mm.CreateNewMonsters(nameTextBox.Text, monsterDescriptionTextBox.Text, monsterTypeTextBox.Text, Convert.ToInt32(threatLevelComboBox.Text), 1, 1, 4);
+            MessageBox.Show("Successfully Created New Monster Entry!");
+            clearBoxes();
+        }
+        private void clearBoxes()
+        {
+            nameTextBox.Text = "";
+            monsterDescriptionTextBox.Text = "";
+            monsterTypeTextBox.Text = "";
+            threatLevelComboBox.SelectedIndex = -1;
         }
     }
 }
